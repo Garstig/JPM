d8605897f87f30fae1bd49d509f1788558fe64a1
Andreas <andiboettcher1992@gmail.com>
2024-12-18 14:36:05 +0100

    color of weekdays is now black

--- frontend/src/components/CalendarHeader.jsx
+++ frontend/src/components/CalendarHeader.jsx
@@ -6,11 +6,12 @@ const CalendarHeader = ({ year, month }) => {
       <h3 className="text-center">{`Calendar for ${year}, Month ${month}`}</h3>
       <div className="row text-center bg-light fw-bold">
         {["MO", "DI", "MI", "DO", "FR", "SA", "SO"].map((day) => (
-          <div className="col border" key={day}>
+          <div className="col border text-black" key={day} style={{ color: 'black !important' }}>
             {day}
           </div>
         ))}
       </div>
+
     </>
   );
 };
